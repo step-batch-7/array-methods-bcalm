be6e6d272b78188141225ed9bc998ef592500f93
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-05-09 16:49:40 +0530

    Added map function

--- array_void.c
+++ array_void.c
@@ -7,9 +7,24 @@ ArrayVoid_ptr create_void_array(int size)
   {
     return NULL;
   }
-  src->array = size ? malloc(size * sizeof(void)) : NULL;
+  src->array = size ? malloc(sizeof(Object)) : NULL;
   src->length = size;
   return src;
+}
+
+ArrayVoid_ptr map_void(ArrayVoid_ptr src, MapperVoid mapper)
+{
+  ArrayVoid_ptr mapped_src = create_void_array(src->length);
+
+  IS_MEMORY_NOT_ALLOCATED(mapped_src)
+  {
+    return mapped_src;
+  }
+  REPEAT(src->length)
+  {
+    mapped_src->array[idx] = (*mapper)(src->array[idx]);
+  }
+  return mapped_src;
 }
 
 Object reduce_void(ArrayVoid_ptr src, Object accumulator, ReducerVoid reducer)
