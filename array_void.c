fce277e5e40614a9b38f8438bdcba1e86f1e1039
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-05-09 16:51:05 +0530

    Added filter function

--- array_void.c
+++ array_void.c
@@ -27,6 +27,29 @@ ArrayVoid_ptr map_void(ArrayVoid_ptr src, MapperVoid mapper)
   return mapped_src;
 }
 
+ArrayVoid_ptr filter_void(ArrayVoid_ptr src, PredicateVoid predicate)
+{
+  int count = 0;
+  ArrayVoid_ptr filtered_src = create_void_array(src->length);
+
+  IS_MEMORY_NOT_ALLOCATED(filtered_src)
+  {
+    return filtered_src;
+  }
+
+  REPEAT(src->length)
+  {
+    if ((*predicate)(src->array[idx]))
+    {
+      filtered_src->array[count] = src->array[idx];
+      count++;
+    }
+  }
+  filtered_src->array = realloc(filtered_src->array, count * sizeof(void));
+  filtered_src->length = count;
+  return filtered_src;
+}
+
 Object reduce_void(ArrayVoid_ptr src, Object accumulator, ReducerVoid reducer)
 {
   REPEAT(src->length)
